3f7341f4a0efaf6f7b37ba6ee4dc4b0f1c880a98
salivala <argosnothing@gmail.com>
2025-10-04 11:08:57 -0400

    should be suitable for an input probably maybe idk

--- flake.nix
+++ flake.nix
@@ -2,24 +2,12 @@
   description = "Occult Themes";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    flake-parts = {
-      url = "github:hercules-ci/flake-parts";
-      inputs.nixpkgs-lib.follows = "nixpkgs";
-    };
   };
 
-  outputs = {
-    flake-parts,
-    nixpkgs,
-    ...
-  } @ inputs:
-    flake-parts.lib.mkFlake {inherit inputs;} {
-      systems = [ "x86_64-linux" "aarch64-linux" ];
-      perSystem = {...}: {
-        flake.lib.themes = {
-          occult-umbral = builtins.fromJSON(builtins ./themes/occult-umbral.json);
-          occult = builtins.fromJSON(builtins.readFile ./themes/occult.json);
-        };
-      };
+  outputs = {...} @ inputs: {
+    themes = {
+      occult-umbral = builtins.fromJSON (builtins.readFile ./themes/occult-umbral.json);
+      occult = builtins.fromJSON (builtins.readFile ./themes/occult.json);
     };
+  };
 }
